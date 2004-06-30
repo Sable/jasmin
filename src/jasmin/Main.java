5a4598d15d081d0d11e02b3ff64261cd0b03a3eb
Ondrej Lhoták <olhotak@uwaterloo.ca>
2004-06-30 22:15:44 +0000

    jasmin 0.4 Thu, 04 May 2000 11:56:55 -0400 by plam (no comment)

--- src/jasmin/Main.java
+++ src/jasmin/Main.java
@@ -19,7 +19,7 @@ public class Main {
     /**
      * The Jasmin version
      */
-    public static final String version = "v1.06-s7";
+    public static final String version = "v1.06-s8";
 
     /**
      * Called to assemble a single file.
