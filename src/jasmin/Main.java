3e842fc72cc4360a4deec52f3b820497d35c6048
Eric Bodden <bodden@ec-spride.de>
2008-04-07 18:53:03 +0000

    fixing up Jasmin version number

--- src/jasmin/Main.java
+++ src/jasmin/Main.java
@@ -54,7 +54,7 @@ public class Main {
     /**
      * The Jasmin version
      */
-    public static final String version = "2.2.3";
+    public static final String version = "2.2.5";
 
     /**
      * Called to assemble a single file.
